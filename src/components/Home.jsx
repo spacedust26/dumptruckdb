a062f4e562f165cdacc07827d3072c1713245463
Pranamya Kulal <spacedust26@gmail.com>
2025-03-29 18:22:58 +0530

    hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,23 @@
+import logobg from '../assets/logo-bg.png';
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='hero-outer'>
+      <div className='hero-inner-left'>
+        <img src={logobg} alt="DumpTruckDB" className="logobg" />
+      </div>
+      <div className='hero-inner-right'>
+        <div>
+          <h1 className="title">Redefining the way you interact with data</h1>
+          <p className="subtitle">DumpTruckDB is your go-to SQL-powered data querying tool. Whether you're handling large datasets or just need a quick way to explore CSV files, we've got you covered. No more messy spreadsheets—upload, write, and execute queries effortlessly! </p>
+        </div>
+        <div>
+          <button>Get Started</button>
+          <a className="github-link" href="https://github.com/spacedust26/dumptruckdb" target="_blank" rel="noopener noreferrer">View on Github →</a>
+
+        </div>
+      </div>
+    </div>
   );
 };
 
