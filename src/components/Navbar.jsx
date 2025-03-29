9acc7982a7ec162c45383bcd7dff9f1e77ea2b08
Pranamya Kulal <spacedust26@gmail.com>
2025-03-29 17:29:36 +0530

    navbar with logo , github link

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import logo from '../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className='nav'>
+        <div className='nav-shine'>
+          <a href="/">
+            <div><img src={logo} className="logo" alt="DumpTruckDB" />
+              <span>DumpTruckDB</span></div>
+          </a>
+        </div>
+        <div>
+          <a href='https://github.com/spacedust26/dumptruckdb' className='git'>Github</a>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar;
